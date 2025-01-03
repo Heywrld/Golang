a279222308e26d4f932046f8021039ef255ad2af
heywrld <drfrabs10@gmail.com>
2025-01-03 12:20:56 +0100

    formatted strings and save formatted strings

--- main.go
+++ main.go
@@ -3,27 +3,17 @@ package main
 import "fmt"
 
 func main()  {
+	age := "25"
+	name := "Ben"
 
-	// strings
-	var nameOne string = "Ben"
+	// printf (formatted strings) %_ = formatt specifier
+	fmt.Printf("my age is %v and my name is %v \n ", age, name)
+	fmt.Printf("my age is %q and my name is %q \n", age, name)
+	fmt.Printf("age is of type %T \n", age)
+	fmt.Printf("you scored %f points! \n", 225.55) // for floats
+	fmt.Printf("you scored %0.1f points! \n", 225.556) //%0.1f = to 1dp and vice versa
 
-	fmt.Println(nameOne)
-
-	//ints
-	var ageOne int = 25
-	ageTwo := 40
-
-	fmt.Println(ageOne, ageTwo)
-
-	//bits & memory
-	var numOne int8 = 25
-	var numTwo uint16 = 200 //uint - cant generate a negative number
-	
-	fmt.Println(numOne, numTwo)
-
-	//float - decimal numbers
-	var scoreOne float32 = 20.22
-	var scoreTwo float64 = 362746234782653627337.9 // we will bw using float64
-
-	fmt.Println(scoreOne, scoreTwo)
+	// sprintf (save formatted strings)
+	var str = fmt.Sprintf("my age is %v and my name is %v \n ", age, name)
+	fmt.Println("the saved string is:", str)
 }
